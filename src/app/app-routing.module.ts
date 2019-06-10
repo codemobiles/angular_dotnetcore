0de717f4a48aeafda623c37cd3096e55c95455b3
iBlurBlur <tanakorn.ngam@gmail.com>
2019-06-10 15:53:54 +0700

    project init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/authen/login/login.component';
+import { RegisterComponent } from './components/authen/register/register.component';
+import { StockHomeComponent } from './components/stock/stock-home/stock-home.component';
+import { StockCreateComponent } from './components/stock/stock-create/stock-create.component';
+import { StockEditComponent } from './components/stock/stock-edit/stock-edit.component';
+import { ShopHomeComponent } from './components/shop/shop-home/shop-home.component';
+import { TransactionHomeComponent } from './components/transaction/transaction-home/transaction-home.component';
+import { TransactionDetailComponent } from './components/transaction/transaction-detail/transaction-detail.component';
+import { ReportComponent } from './components/report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'stock', component: StockHomeComponent},
+  { path: 'stock/create', component: StockCreateComponent},
+  { path: 'stock/edit/:id', component: StockEditComponent},
+  { path: 'shop', component: ShopHomeComponent},
+  { path: 'transaction', component: TransactionHomeComponent },
+  { path: 'transaction/detail/:id', component: TransactionDetailComponent },
+  { path: 'report', component: ReportComponent },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth/login' }, // for page not found
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/authen/login/login.component';
+import { RegisterComponent } from './components/authen/register/register.component';
+import { ReportComponent } from './components/report/report.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FotterComponent } from './components/shared/fotter/fotter.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { ShopHomeComponent } from './components/shop/shop-home/shop-home.component';
+import { ShopPaymentComponent } from './components/shop/shop-payment/shop-payment.component';
+import { StockCreateComponent } from './components/stock/stock-create/stock-create.component';
+import { StockEditComponent } from './components/stock/stock-edit/stock-edit.component';
+import { StockHomeComponent } from './components/stock/stock-home/stock-home.component';
+import { TransactionDetailComponent } from './components/transaction/transaction-detail/transaction-detail.component';
+import { TransactionHomeComponent } from './components/transaction/transaction-home/transaction-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ReportComponent,
+    HeaderComponent,
+    FotterComponent,
+    MenuComponent,
+    ShopHomeComponent,
+    ShopPaymentComponent,
+    StockCreateComponent,
+    StockEditComponent,
+    StockHomeComponent,
+    TransactionDetailComponent,
+    TransactionHomeComponent
   ],
   imports: [
     BrowserModule,
